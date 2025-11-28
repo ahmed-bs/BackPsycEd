a4a5df7747125c5b4ed1eb4bc46c51199238f61b
ahmed-bs <medamed777@gmail.com>
2025-11-28 15:01:46 +0100

    ! from pwd

--- backend/settings.py
+++ backend/settings.py
@@ -147,7 +147,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'evaluation'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'your_secure_password_here'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '!'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
